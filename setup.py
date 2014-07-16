4a0fc5f43822b2cd71eda31fd16ccf94e11f7691
Sam Dolan <samdolan@gmail.com>
2014-07-16 14:55:01 -0700

    Parse requirements file

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup
 
+
+def parse_requirements(requirements_path):
+    """Parse a requirements.txt file as a list of requirements."""
+    with open(requirements_path) as f:
+        return [l.strip('\n') for l in f
+                if l.strip('\n') and not l.startswith('#')]
+
+requirements = parse_requirements('requirements.txt')
+
 setup(
     name='FlaskPlus',
     version='0.1',
@@ -7,5 +16,5 @@ setup(
     packages=['flaskplus'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask']
+    install_requires=requirements,
 )
